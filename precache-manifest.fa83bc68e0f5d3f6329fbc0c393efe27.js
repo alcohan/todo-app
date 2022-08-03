530ed09a803bf3f485e3eaa5d047fd8c129a2571
unknown <adu.cohan@gmail.com>
2022-08-03 16:12:52 -0700

    Updates

---
+++ precache-manifest.fa83bc68e0f5d3f6329fbc0c393efe27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "28c2182d86880a32e40d",
+    "url": "/todo-app/static/css/main.b6da2bec.chunk.css"
+  },
+  {
+    "revision": "28c2182d86880a32e40d",
+    "url": "/todo-app/static/js/main.28c2182d.chunk.js"
+  },
+  {
+    "revision": "266190d1fb51ad12d0a4",
+    "url": "/todo-app/static/js/1.266190d1.chunk.js"
+  },
+  {
+    "revision": "60cb8325f0856b1718a5",
+    "url": "/todo-app/static/js/2.60cb8325.chunk.js"
+  },
+  {
+    "revision": "53aabd24eac300c71093",
+    "url": "/todo-app/static/js/runtime~main.53aabd24.js"
+  },
+  {
+    "revision": "bacb75159c32516be1d9a2c04e3dc76c",
+    "url": "/todo-app/index.html"
+  }
+];
